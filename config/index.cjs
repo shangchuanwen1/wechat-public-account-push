0b0feb82b38f2ecfa8e159c859e8adf3b5bd3690
shangchuanwen1 <66344508+shangchuanwen1@users.noreply.github.com>
2022-10-10 08:43:18 -0500

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -58,7 +58,7 @@ USE_PASSAGE:'push-deer',
      
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU16764TV9MHREEi86z3lmQQR8ypCta4vgSiujLW',
     }
   ],
 
