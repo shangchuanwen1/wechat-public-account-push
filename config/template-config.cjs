dd1df205f553e2a8911610f419c72c84cfb99883
shangchuanwen1 <66344508+shangchuanwen1@users.noreply.github.com>
2022-10-10 09:47:50 -0500

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,11 +2,10 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '亲爱的欣儿, 早上好',
     desc: `
       **{{date.DATA}}**
       下个休息日：{{holidaytts.DATA}}
-      ---
       城市：{{city.DATA}}
       天气：{{weather.DATA}}
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
@@ -14,7 +13,7 @@ const TEMPLATE_CONFIG = [
       风级: {{wind_scale.DATA}}
       {{comprehensive_horoscope.DATA}}
       ---
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们恋爱的第{{love_day.DATA}}天
       {{birthday_message.DATA}}
       ---
       {{moment_copyrighting.DATA}}
