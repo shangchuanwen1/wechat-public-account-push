00ef55fee81430d184a65e70a91c90c6be108610
shangchuanwen1 <66344508+shangchuanwen1@users.noreply.github.com>
2022-10-23 20:17:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ USE_PASSAGE:'push-deer',
       // 想要发送的人的名字
       name: '欣儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxd393857a0dcf2fac',
+      id: 'PDU16764TV9MHREEi86z3lmQQR8ypCta4vgSiujLW',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ USE_PASSAGE:'push-deer',
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxd393857a0dcf2fac',
+      id: 'PDU16764TV9MHREEi86z3lmQQR8ypCta4vgSiujLW',
     }
   ],
 
